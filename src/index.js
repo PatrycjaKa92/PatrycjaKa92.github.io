503c55071782c2bf1fff277d77b89621a1764cf7
PatrycjaKa92 <58090630+PatrycjaKa92@users.noreply.github.com>
2020-04-12 22:19:00 +0200

    Praca domowa nr 1

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import 'src/styles/style.sass'
+
+import script from 'src/script.js'
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.sass$/,
+                exclude: /node_modules/,
+                use: ['sass-loader']
+            },
+            {
+                test: /\.png$/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+}
+import
